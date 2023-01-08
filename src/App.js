e3ee926c49d4a655a78b54f3c69840a885db6d2b
relmgren <elmgren93@gmail.com>
2023-01-08 13:53:21 +0100

    update description

--- src/App.js
+++ src/App.js
@@ -17,17 +17,18 @@ function App() {
         <h3 className="Huvudtext">Details</h3>
         <div className="Detail-section">
           <h3>Time and location</h3>
-          <p>Saturday 4th February 18:00</p>
+          <p>Saturday 4th February 18:15</p>
           <p>Skeppargatan 80, 11459 Stockholm</p>
           <h3>Theme and dresscode</h3>
           <img src={shakingAlice} className="Background-alice" alt="falling-alice" />
           <p>Alice in Wonderland - A Mad Tea Party</p>
-          <p>Creative black tie</p>
+          <p>Creative cocktail</p>
           <h3>RSVP</h3>
-          <p>Dinner and drinks will be served</p>
+          <p>Light dinner and drinks will be served</p>
+          <p>Please bring your own crazy tea cup for the tea party</p>
           <p>I love your kids and dogs but please don't bring them with you to the mad house</p>
-          <p>Please notify if you will not come</p>
-          <h3>Watch the movie for the full experience</h3>
+          <p>Please notify if you will attend</p>
+          <h3>Watch or read Alice in Wonderland for the full experience</h3>
         </div>
       </div>
       <div className="App-details">
